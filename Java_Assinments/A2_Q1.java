d9e50ef2d4d70ad2908565f53eb913645e9511ff
musheer2911 <musheer2911@gmail.com>
2024-08-27 18:35:20 +0530

    Question 2 is done on same file

--- Java_Assinments/A2_Q1.java
+++ Java_Assinments/A2_Q1.java
@@ -1,9 +1,12 @@
+// Question 1.2: Second commit after adding this method to the code.
+// Create a method for the above class as setInfo(), accepting parameters(name, age, address)
+// and update the values of the instance variable in this method using this keyword.
 import java.util.*;
 
 class person 
 {
 	int age;
-	String name, Address;
+	String name, address;
 	person()
 	{			
 
@@ -12,19 +15,27 @@ class person
 	{	
 		name = n;
 		age = ag;
-		Address = ad;
+		address = ad;
 	}
 	public void displayInfo()
 	{
 		System.out.println("Displaying info......\n ");
-		System.out.println("Name: " + name + ".\nAge: " + age +".\nAddress: "+ Address +'\n');
+		System.out.println("Name: " + name + ".\nAge: " + age +".\nAddress: "+ address +'\n');
 	}
+	public void setInfo(String name,int age,String address)
+	{
+		this.name = name;
+		this.age = age;
+		this.address = address;
+	}
+	
 	public static void main(String[] args)
 	{
 		person obj = new person();
 		person obj2 = new person("mak",20,"nanded");
 		obj2.displayInfo();
-		
+		obj2.setInfo("musheer",24,"maharashtra");
+		obj2.displayInfo();
 	}
 	
 }
