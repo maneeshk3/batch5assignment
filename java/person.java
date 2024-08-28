d16619828efb981302e291a55229866505b290e5
surya123659 <suryakiran8465@gmail.com>
2024-08-28 21:44:15 +0530

    submitting SQL assignment

--- java/person.java
+++ java/person.java
@@ -1,57 +1,37 @@
-import java.util.Scanner;
-public class Person {
-    // Instance variables
-    private String name;
-    private int age;
-    private String address;
 
-    // Default constructor
-    public Person() {
-        this.name = "Unknown";
-        this.age = 0;
-        this.address = "Unknown";
-    }
+import java.util.*;
+class person
+{ 
+ int age;
+ String name, address;
 
-    // Parameterized constructor
-    public Person(String name, int age, String address) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
+ 
+ person() {
+ 
+  age = 24;
+  name ="Nani";
+  address = "Vizag";
+ }
 
-    // Method to display person details
-    public void displayInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Address: " + address);
-    }
+ person( String name1, int age1, String address1)
+ {
+	name = name1;
+	age = age1;
+	address = address1;
+ }
 
-    // Method to set person info
-    public void setInfo(String name, int age, String address) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
+ public void per(){
+ System.out.println(name + '\n'+ age +'\n' + address);
+ 
+ }
 
-    // Method to check if the person is an adult
-    public boolean isAdult() {
-        return this.age >= 18;
-    }
+public static void main (String args[])
+{
+person newperson1 = new person();
+person newperson2 = new person("Dinesh", 22, "Mumbai");
 
-    public static void main(String[] args) {
-        // Creating objects using both constructors
-        Person person1 = new Person(); // Default constructor
-        Person person2 = new Person("Alice", 30, "123 Main St"); // Parameterized constructor
-
-        // Displaying information
-        person1.displayInfo();
-        person2.displayInfo();
-
-        // Setting new info for person1
-        person1.setInfo("Bob", 25, "456 Elm St");
-        person1.displayInfo();
-
-        // Checking if person2 is an adult
-        System.out.println("Is person2 an adult? " + person2.isAdult());
-    }
+ newperson1.per();
+ newperson2.per();
+ 
+}
 }
