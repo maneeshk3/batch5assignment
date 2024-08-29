9c299803fef69c6558abb976c70f8bede087a58e
RohitAtnoor <rohitatnoor12@gmail.com>
2024-08-29 22:54:39 +0530

    Question23SQL

--- Person.java
+++ Person.java
@@ -3,6 +3,7 @@ public class Person{
 	String First_Name,Last_Name,Address,Email,Gender;
 	int Age;
 	
+	//Question 1		 
 	public Person(){
 		this.First_Name = "Rohit";
 		this.Last_Name = "Rohit";
@@ -12,6 +13,7 @@ public class Person{
 		this.Age = 26;
 	}
 	
+	//Question 1		 
 	public Person(String First_Name,String Last_Name,String Address,String Email,String Gender,int Age){
 		this.First_Name = First_Name;
 		this.Last_Name = Last_Name;
@@ -21,16 +23,48 @@ public class Person{
 		this.Age = Age;
 	}
 	
+	//Question 1		 
 	public void displayInfo(){
 		System.out.println("The details of the person are as Shown Below");
 		System.out.println(First_Name +"\n"+ Last_Name +"\n"+ Address +"\n"+ Email +"\n"+ Gender +"\n"+ Age);
 	}
+	
+	//Question 2
+	public void setinfo(String firstname,String lastname, String address,String mail,String gender,int age){
+		this.First_Name = firstname;
+		this.Last_Name = lastname;
+		this.Address = address;
+		this.Email = mail;
+		this.Gender = gender;
+		this.Age = age;
+	}
+
+	//Question 3
+	public boolean isAdult(){
+		return this.Age >18;
+	}
+	
 	
 	public static void main(String[] args){
 		Person User1 = new Person();
 		Person User2 = new Person("ABC","XYZ","IN","Gmail","Male",25);
 		User1.displayInfo();
 		User2.displayInfo();
+		System.out.println("Question 1 Completed");
+		System.out.println("*".repeat(100));
+
+		System.out.println("Updating the user details.");
+		User2.setinfo("google", "java", "india", "gmail.com", "M", 26);
+		User2.displayInfo();
+
+		System.out.println("Details updated Question 2 completed");
+		System.out.println("*".repeat(100));
+
+		System.out.println("Audit check of the age");
+		User1.isAdult();
+		User2.isAdult();
+		System.out.println("Audit check of the age completed");
+		System.out.println("*".repeat(100));
 	}
 	
 }
