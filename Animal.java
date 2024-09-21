951d5b31109ce9a549d9b9eceb3bc45e0df8e418
NagendraSatish <153088051+NagendraSatish@users.noreply.github.com>
2024-09-21 06:16:59 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,41 @@
+public Class Animal{
+	void makeSound(){
+		System.out.println("The animal makes a sound.");
+	}
+}
+public Class Dog extends Animal{
+	@Override
+    
+    public void makeSound() {
+        
+        System.out.println("The dog sounds woof.");
+}
+	
+}
+
+public Class Cat extends Animal{
+	@Override
+    
+    public void makeSound() {
+        
+        System.out.println("The cat sounds Meow");
+}
+
+public static void main String[]args{
+	 Animal animal = new Animal();
+        
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        animal.makeSound();
+        // Call the makeSound method on the cat instance
+        cat.makeSound();
+		dog.makeSound();
+		
+	
+	
+	
+	
+	
+
+
+}
