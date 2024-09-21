3dfa9886ffa86a8a83af1baa12481f4915f963b1
NagendraSatish <153088051+NagendraSatish@users.noreply.github.com>
2024-09-21 08:13:49 +0530

    Update AnimalTest.java

--- AnimalTest.java
+++ AnimalTest.java
@@ -21,7 +21,7 @@ public Class Cat extends Animal{
         System.out.println("The cat sounds Meow");
 }
 public class AnimalTest {
-public static void main String[]args{
+public static void main (String[]args){
 	 Animal animal = new Animal();
         
         Cat cat = new Cat();
