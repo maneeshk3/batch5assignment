ecc99a025af9299541e9e61a47a71fccde013f3d
NagendraSatish <153088051+NagendraSatish@users.noreply.github.com>
2024-09-23 07:36:28 +0530

    Add files via upload

---
+++ PersonCons .java
@@ -0,0 +1,42 @@
+public class PersonCons {
+
+    // Instance variables
+    private String name;
+    private int age;
+    private String address;
+
+    // Default constructor
+    public Person() {
+        this.name = "Unknown";
+        this.age = 0;
+        this.address = "Unknown";
+    }
+
+    // Parameterized constructor
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    // Method to display person details
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+    }
+
+    // Main method to create and display Person objects
+    public static void main(String[] args) {
+        // Create Person object using the default constructor
+        Person person1 = new Person();
+        System.out.println("Person 1 Details:");
+        person1.displayInfo();
+        System.out.println();
+
+        // Create Person object using the parameterized constructor
+        Person person2 = new Person("John Doe", 30, "123 Main Street");
+        System.out.println("Person 2 Details:");
+        person2.displayInfo();
+    }
+}
